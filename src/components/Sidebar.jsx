55046b0518a8d7234bee3a072d289e540c766265
Singh-Ishu <itzshaansingh@gmail.com>
2025-02-28 12:18:50 +0530

    Made the mandelbrot generator functional

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,65 +1,60 @@
 import "./Sidebar.css";
+import { updateFractalParams } from "../utils/Mandelbrot";
 
-export default function Sidebar() {
+export default function Sidebar({ onGenerate }) {
   return (
     <div className="sidebar">
-      {/* Fractal Calculation Section */}
       <div id="Fractal-Calculation">
         <table>
-          <tbody>
-            <tr>
-              <td>
-                <label htmlFor="zr">Z(r)</label>
-              </td>
-              <td>
-                <input type="number" id="zr" name="zr" defaultValue="0" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="zi">Z(i)</label>
-              </td>
-              <td>
-                <input type="number" id="zi" name="zi" defaultValue="0" />
-              </td>
-            </tr>
-          </tbody>
+          <tr>
+            <td>
+              <label htmlFor="zr">Z(r)</label>
+            </td>
+            <td>
+              <input type="number" id="zr" name="zr" placeholder="0" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="zi">Z(i)</label>
+            </td>
+            <td>
+              <input type="number" id="zi" name="zi" placeholder="0" />
+            </td>
+          </tr>
         </table>
       </div>
 
-      {/* Fractal Appearance Section */}
       <div id="Fractal-Appearance">
         <table>
-          <tbody>
-            <tr>
-              <td>
-                <label htmlFor="r">Red Multiplier</label>
-              </td>
-              <td>
-                <input type="number" id="r" name="r" defaultValue="1" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="g">Green Multiplier</label>
-              </td>
-              <td>
-                <input type="number" id="g" name="g" defaultValue="1" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="b">Blue Multiplier</label>
-              </td>
-              <td>
-                <input type="number" id="b" name="b" defaultValue="1" />
-              </td>
-            </tr>
-          </tbody>
+          <tr>
+            <td>
+              <label htmlFor="r">Red Multiplier:</label>
+            </td>
+            <td>
+              <input type="number" id="r" name="r" defaultValue="1" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="g">Green Multiplier:</label>
+            </td>
+            <td>
+              <input type="number" id="g" name="g" defaultValue="1" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="b">Blue Multiplier:</label>
+            </td>
+            <td>
+              <input type="number" id="b" name="b" defaultValue="1" />
+            </td>
+          </tr>
         </table>
       </div>
 
-      <button>Generate</button>
+      <button onClick={updateFractalParams}>Generate</button>
     </div>
   );
 }
