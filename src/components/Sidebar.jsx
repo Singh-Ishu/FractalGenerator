fbec2afd7fa7f8c56258a51cf888f552238bfae6
Singh-Ishu <itzshaansingh@gmail.com>
2025-02-28 08:48:06 +0530

    Created components, added css

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div>
+      <div>
+        <label for="xc"></label>
+        <input type="number" name="xc" placeholder="0"></input>
+        <label for="yc"></label>
+        <input type="number" name="yc" placeholder="0"></input>
+      </div>
+      <div></div>
+    </div>
+  );
+}
