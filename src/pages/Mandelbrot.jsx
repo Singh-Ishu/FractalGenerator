a4476922ee41811074449d98826bcd94273fef8d
Singh-Ishu <itzshaansingh@gmail.com>
2025-03-21 20:50:30 +0530

    Added Mandelbrot Introductory text

--- src/pages/Mandelbrot.jsx
+++ src/pages/Mandelbrot.jsx
@@ -12,9 +12,30 @@ export default function Mandelbrot() {
   }, [location.pathname]); // Depend on the route, so it re-renders
 
   return (
-    <div id="container">
-      <Sidebar />
-      <canvas id="drawing-board" onLoad={renderMandelbrot()}></canvas>
-    </div>
+    <>
+      <div id="container">
+        <Sidebar />
+        <canvas id="drawing-board" onLoad={renderMandelbrot()}></canvas>
+      </div>
+      <div>
+        <p>
+          The Mandelbrot Set is one of the most famous fractals, known for its
+          infinite complexity and self-repeating patterns. It is generated using
+          a simple mathematical formula:{" "}
+          <b>
+            z<sub>n+1</sub> = z<sub>n</sub>
+            <sup>2</sup> + c
+          </b>{" "}
+          where z and c are complex numbers.
+          <br />
+          By iterating this equation and tracking how fast the values escape to
+          infinity, we can visualize the intricate and mesmerizing structures of
+          the Mandelbrot Set. This page renders the Mandelbrot Set using WebGL,
+          allowing for smooth zooming and real-time interaction. Explore deeper,
+          and you'll discover endless variations of patterns hidden within the
+          fractal, revealing nature's deep mathematical beauty.
+        </p>
+      </div>
+    </>
   );
 }
