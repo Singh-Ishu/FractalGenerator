d69c3241c5e6673ee58afba3639935760f8cfc9f
Singh-Ishu <itzshaansingh@gmail.com>
2025-03-22 23:54:37 +0530

    Finished templating for Burnign ship.

--- src/pages/BurningShip.jsx
+++ src/pages/BurningShip.jsx
@@ -1,1 +1,27 @@
-export default function BurningShip() {}
+export default function BurningShip() {
+  return (
+    <>
+      <div>
+        <p>
+          The <strong>Burning Ship fractal</strong> is a chaotic and mesmerizing
+          fractal variation that follows a modification of the Mandelbrot
+          equation. Instead of using the standard complex squaring operation, it
+          applies absolute values to the real and imaginary components
+          separately:
+        </p>
+        <p>
+          z<sub>n+1</sub> = (|Re(z<sub>n</sub>)| + i |Im(z<sub>n</sub>)|)
+          <sup>2</sup> + c
+        </p>
+        <p>
+          This small tweak results in a fractal with a{" "}
+          <strong>sharp, jagged, ship-like appearance</strong>, in contrast to
+          the smooth curves of the Mandelbrot Set. The fractal often resembles a
+          turbulent seascape, giving it the name "Burning Ship." By zooming into
+          different regions, one can uncover a landscape of branching structures
+          and intricate, chaotic formations.
+        </p>
+      </div>
+    </>
+  );
+}
