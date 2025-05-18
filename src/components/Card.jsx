b88b627fd692493a556f8010c288cb255b5ce116
Singh-Ishu <itzshaansingh@gmail.com>
2025-05-18 16:39:09 +0530

    Added Cards to Fractal2d

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
+import "./Card.css";
+
 export default function Card({ Name, Equation, Image_URL }) {
     return (
         <div className="card-container">
-            <img src={Image_URL} alt={Name} />
-            <h1>{Name}</h1>
-            <h2>Equation: {Equation}</h2>
+            <img src={Image_URL} alt={Name} height={300} />
+            <div className="card-text">
+                <h1>{Name}</h1>
+                <h2>Equation: {Equation}</h2>
+            </div>
         </div>
     );
 }
--- src/pages/Fractals2d.jsx
+++ src/pages/Fractals2d.jsx
@@ -1,3 +1,23 @@
+import Card from "../components/Card";
+
 export default function Fractals2d() {
-    return <></>;
+    return (
+        <>
+            <Card
+                Name="Mandelbrot"
+                Equation={1}
+                Image_URL={"src/assets/PlainMandelbrot.png"}
+            />
+            <Card
+                Name="Julia"
+                Equation={1}
+                Image_URL={"src/assets/PlainJulia.png"}
+            />
+            <Card
+                Name="Burning-Ship"
+                Equation={1}
+                Image_URL={"src/assets/PlainBurningShip.png"}
+            />
+        </>
+    );
 }
