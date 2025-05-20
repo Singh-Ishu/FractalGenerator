716267fac474b318a19d1002120f9235ae629877
Singh-Ishu <itzshaansingh@gmail.com>
2025-05-20 19:42:07 +0530

    Fixed images for ghpages deployment

--- src/pages/Fractals2d.jsx
+++ src/pages/Fractals2d.jsx
@@ -6,6 +6,11 @@
 import Card from "../components/Card";
 import "./fractalinfo.css";
 
+//Import fractal images
+import PlainMandelbrot from "../assets/PlainMandelbrot.png";
+import PlainJulia from "../assets/PlainJulia.png";
+import PlainBurningShip from "../assets/PlainBurningShip.png";
+
 /**
  * Array of 2D fractal information
  * @type {Array<{name: string, equation: string, imageUrl: string, path?: string}>}
@@ -14,36 +19,36 @@ const FRACTALS_2D = [
     {
         name: "Mandelbrot",
         equation: "z_n+1 = z_n^2 + c",
-        imageUrl: "src/assets/PlainMandelbrot.png",
-        path: "/2d/mandelbrot"
+        imageUrl: PlainMandelbrot,
+        path: "/2d/mandelbrot",
     },
     {
         name: "Julia",
         equation: "z_n+1 = z_n^2 + c",
-        imageUrl: "src/assets/PlainJulia.png",
-        path: "/2d/julia"
+        imageUrl: PlainJulia,
+        path: "/2d/julia",
     },
     {
         name: "Burning Ship",
         equation: "z_n+1 = (|Re(z_n)| + i|Im(z_n)|)^2 + c",
-        imageUrl: "src/assets/PlainBurningShip.png",
-        path: "/2d/burning-ship"
+        imageUrl: PlainBurningShip,
+        path: "/2d/burning-ship",
     },
     {
         name: "Sierpinski Triangle (W.I.P)",
         equation: "Fractal generated through recursive subdivision",
-        imageUrl: "src/assets/sierpinski.png"
+        imageUrl: "src/assets/sierpinski.png",
     },
     {
         name: "Koch Snowflake (W.I.P)",
         equation: "Fractal generated through recursive edge replacement",
-        imageUrl: "src/assets/koch.png"
+        imageUrl: "src/assets/koch.png",
     },
     {
         name: "Barnsley Fern (W.I.P)",
         equation: "Generated using iterated function system (IFS)",
-        imageUrl: "src/assets/fern.png"
-    }
+        imageUrl: "src/assets/fern.png",
+    },
 ];
 
 export default function Fractals2d() {
--- src/pages/Fractals3d.jsx
+++ src/pages/Fractals3d.jsx
@@ -6,6 +6,9 @@
 import Card from "../components/Card";
 import "./fractalinfo.css";
 
+//Import fractal images
+import PlainMandelbulb from "../assets/PlainMandelbulb.png";
+
 /**
  * Array of 3D fractal information
  * @type {Array<{name: string, equation: string, imageUrl: string, path?: string}>}
@@ -14,24 +17,24 @@ const FRACTALS_3D = [
     {
         name: "Mandelbulb",
         equation: "z_n+1 = z_n^8 + c",
-        imageUrl: "src/assets/PlainMandelbulb.png",
-        path: "/3d/mandelbulb"
+        imageUrl: PlainMandelbulb,
+        path: "/3d/mandelbulb",
     },
     {
         name: "Menger Sponge (W.I.P)",
         equation: "3D generalization of the Sierpinski carpet",
-        imageUrl: "src/assets/menger.png"
+        imageUrl: "src/assets/menger.png",
     },
     {
         name: "Quaternion Julia (W.I.P)",
         equation: "z_n+1 = z_n^2 + c, where z,c ∈ ℍ",
-        imageUrl: "src/assets/quaternion.png"
+        imageUrl: "src/assets/quaternion.png",
     },
     {
         name: "Kleinian Groups (W.I.P)",
         equation: "Generated through Möbius transformations",
-        imageUrl: "src/assets/kleinian.png"
-    }
+        imageUrl: "src/assets/kleinian.png",
+    },
 ];
 
 export default function Fractals3d() {
