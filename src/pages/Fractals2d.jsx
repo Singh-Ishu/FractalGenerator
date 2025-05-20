915ad03b02bf915c5a7600f45ad88f00ccbf62b4
Singh-Ishu <itzshaansingh@gmail.com>
2025-05-20 19:14:10 +0530

    Finished spacing

--- src/pages/Fractals2d.jsx
+++ src/pages/Fractals2d.jsx
@@ -1,23 +1,26 @@
 import Card from "../components/Card";
+import "./fractalinfo.css"
 
 export default function Fractals2d() {
     return (
-        <div className="card-carousel">
-            <Card
-                Name="Mandelbrot"
-                Equation={1}
-                Image_URL={"src/assets/PlainMandelbrot.png"}
-            />
-            <Card
-                Name="Julia"
-                Equation={1}
-                Image_URL={"src/assets/PlainJulia.png"}
-            />
-            <Card
-                Name="Burning-Ship"
-                Equation={1}
-                Image_URL={"src/assets/PlainBurningShip.png"}
-            />
+        <div className="fractal-info-page-container">
+            <div className="card-carousel">
+                <Card
+                    Name="Mandelbrot"
+                    Equation={1}
+                    Image_URL={"src/assets/PlainMandelbrot.png"}
+                />
+                <Card
+                    Name="Julia"
+                    Equation={1}
+                    Image_URL={"src/assets/PlainJulia.png"}
+                />
+                <Card
+                    Name="Burning-Ship"
+                    Equation={1}
+                    Image_URL={"src/assets/PlainBurningShip.png"}
+                />
+            </div>
         </div>
     );
 }
--- src/pages/Fractals3d.jsx
+++ src/pages/Fractals3d.jsx
@@ -1,12 +1,15 @@
 import Card from "../components/Card";
+import "./fractalinfo.css" 
 
 export default function Fractals3d() {
     return (
-        <div className="card-carousel">
-            <Card
-                Name={"Mandelbulb"}
-                Image_URL={"src/assets/PlainMandelbulb.png"}
-            />
+        <div className="fractal-info-page-container">
+            <div className="card-carousel">
+                <Card
+                    Name={"Mandelbulb"}
+                    Image_URL={"src/assets/PlainMandelbulb.png"}
+                />
+            </div>
         </div>
     );
 }
